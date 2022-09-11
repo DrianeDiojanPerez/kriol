804f07f08dea9e3d704aab568893ec169b9bf742
Driane Diojan Perez <drianeperez12@gmail.com>
2022-09-10 19:44:59 -0600

    installing a third party router

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-func (app *application) healcheckHandler(w http.ResponseWriter, r *http.Request){
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w, "status: avaialble")
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n",version)
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -39,11 +39,11 @@ func main() {
 	}
 	//Create our new servermux
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healcheckHandler)
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 	//create our Http server
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: mux,
+		Handler: app.routes(),// using the routes function from routes,go
 		IdleTimeout: time.Minute,
 		ReadTimeout: 10 * time.Second,
 		WriteTimeout: 30 * time.Second,
