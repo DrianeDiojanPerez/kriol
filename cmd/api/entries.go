44052ef08e72dc6bbed78358ea30dbb54503ab17
Driane Diojan Perez <drianeperez12@gmail.com>
2022-09-10 20:33:05 -0600

    moved the code that check for the id parameter into its own method readIDparam

--- cmd/api/entries.go
+++ cmd/api/entries.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 //create entires hander for the POST /v1/entries endpoint
 func (app *application) createEntryHandler(w http.ResponseWriter, r *http.Request){
@@ -13,14 +10,12 @@ func (app *application) createEntryHandler(w http.ResponseWriter, r *http.Reques
 }
 //create showentires hander for the GET /v1/entries/:id endpoint
 func (app *application) showEntryHandler(w http.ResponseWriter, r *http.Request){
-	//use the "paramsfromcontext()" function to get the request context a slice
-	params := httprouter.ParamsFromContext(r.Context())
-	//GET the value of the "id" parameter
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
+	
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w,r)
 		return
 	}
 	// Display the entires
-	fmt.Fprintf(w, "Show The Details For Emtry %d\n", id)
+	fmt.Fprintf(w, "Show The Details For Entry %d\n", id)
 }
