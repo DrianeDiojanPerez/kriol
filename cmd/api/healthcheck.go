41bf6139c4f494ce619d09e9a596b34d939195d0
Driane Diojan Perez <drianeperez12@gmail.com>
2022-09-11 20:13:35 -0600

    jason section added

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,13 +2,22 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "status: avaialble")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n",version)
+
+	// Create a map to hold the healthcheck data
+	data := map[string]string{
+		"status":      "available",
+		"enviornment": app.config.env,
+		"version":     version,
+	}
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -18,4 +19,23 @@ func (app *application) readIDParam(r *http.Request) (int64, error){
 		return 0, errors.New("Invalid Id Parameter")
 	}
 	return id, nil
+}
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	//Convert our map into a JSON object
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// Add a newline to make viewing on the terminal easier
+	js = append(js, '\n')
+	// Add the headers
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	// Specify that we will serve our responses using JSON
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	// Write the []byte slice containing the JSON response body
+	w.Write(js)
+	return nil
 }
