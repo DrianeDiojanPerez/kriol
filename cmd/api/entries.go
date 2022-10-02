b18556af94b84e5e75fc1bba36bd2b7dbea2b63c
Driane Diojan Perez <drianeperez12@gmail.com>
2022-10-02 12:32:01 -0600

    JSON requests I

--- cmd/api/entries.go
+++ cmd/api/entries.go
@@ -1,12 +1,29 @@
 // Filename: cmd/api/entries.go
 package main
+
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
 //create entires hander for the POST /v1/entries endpoint
 func (app *application) createEntryHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Create a New Entry")
+	//our target decode destination
+	var input struct{
+		Name string `json:"name"`
+		String string `json:"string"`
+		Translate string `json:"translate"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	//Display the request
+	fmt.Fprintf(w, "%+v\n", input)
+
+	//fmt.Fprintln(w, "Create a New Entry")
 }
 //create showentires hander for the GET /v1/entries/:id endpoint
 func (app *application) showEntryHandler(w http.ResponseWriter, r *http.Request){
